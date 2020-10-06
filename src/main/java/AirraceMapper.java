90edd2d71e23a88ba68af2277e2ce8c45e718883
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 02:39:40 +0300

    Scripted auto-commit on change (2020-10-07 02:39:40) by gitwatch.sh

--- src/main/java/AirraceMapper.java
+++ src/main/java/AirraceMapper.java
@@ -1,11 +1,13 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 import javax.naming.Context;
 import java.io.IOException;
 
-public class AirraceMapper extends Mapper <LongWritable, Text, TextPair , Text> {
+public class AirraceMapper extends Mapper<LongWritable, Text, LongWritable , Text> {
     @Override
+    public static String fileTag = 
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
     }
