7821d39d606df2dc0e40301bfbdbf36baeb7693e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 01:31:47 +0300

    Scripted auto-commit on change (2020-10-07 01:31:47) by gitwatch.sh

--- src/main/java/AiroportWritable.java
+++ src/main/java/AiroportWritable.java
@@ -6,6 +6,7 @@ public class AiroportWritable implements Writable {
     private IntWritable quarter;
     private IntWritable month;
     private IntWritable day_of_month;
+    private IntWritable day_of_week;
     private 
 
 }
