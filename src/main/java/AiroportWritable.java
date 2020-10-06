b173c6e9908d2556863fafdc15bfc5d122e6b132
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 01:57:55 +0300

    Scripted auto-commit on change (2020-10-07 01:57:55) by gitwatch.sh

--- src/main/java/AiroportWritable.java
+++ src/main/java/AiroportWritable.java
@@ -1,3 +1,4 @@
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
@@ -8,10 +9,10 @@ import java.io.IOException;
 
 public class AiroportWritable implements Writable {
     private IntWritable year , quarter , month , day_of_month , day_of_week , fl_num,
-            origin_airoport_id , orig_airpor_seq_id , origin_cyti_id , dest_airoprt_id,
-            ;
+            origin_airoport_id , orig_airpor_seq_id , origin_cyti_id , dest_airoprt_id;
     private Text fl_date , uniq_carrier , airlane_id , carrier , tail_num , wheels_on ,
-            airr_time , 
+            airr_time;
+    private DoubleWritable 
 
     @Override
     public void write(DataOutput out) throws IOException {
