19c8e89403ba350d026bf4d9c5d6aef2edf48630
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 00:06:11 +0300

    Scripted auto-commit on change (2020-10-07 00:06:11) by gitwatch.sh

--- src/main/java/MainAiroportApp.java
+++ src/main/java/MainAiroportApp.java
@@ -1,3 +1,9 @@
 public class MainAiroportApp {
-    
+    public static void main(String arg[]) throws Exception{
+        if (arg.length < 2){
+            System.err.println("MainAiroportApp exeptiom");
+            System.exit(1);
+        }
+        
+    }
 }
