4fb06021b806886e3d68f07f553f7324f58b5c3c
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 00:08:35 +0300

    Scripted auto-commit on change (2020-10-07 00:08:35) by gitwatch.sh

--- src/main/java/MainAiroportApp.java
+++ src/main/java/MainAiroportApp.java
@@ -1,3 +1,5 @@
+import org.apache.hadoop.mapreduce.Job;
+
 public class MainAiroportApp {
     public static void main(String arg[]) throws Exception{
         if (arg.length < 2){
@@ -5,6 +7,6 @@ public class MainAiroportApp {
             System.exit(1);
         }
 //        gitwatch cheack???
-        
+        Job job = Job.getInstance();
     }
 }
