970a321efdc2fae32301a361765cb3e1d1908153
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 02:19:55 +0300

    Scripted auto-commit on change (2020-10-07 02:19:55) by gitwatch.sh

--- src/main/java/AirraceMapper.java
+++ src/main/java/AirraceMapper.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
-public class AirraceMapper extends Mapper <LongWritable  {
+public class AirraceMapper extends Mapper <LongWritable, Text,  {
 }
