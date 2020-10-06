0eb3001010aef038ca16d7533bcefc7f52d00421
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 01:46:07 +0300

    Scripted auto-commit on change (2020-10-07 01:46:07) by gitwatch.sh

--- src/main/java/AiroportWritable.java
+++ src/main/java/AiroportWritable.java
@@ -6,7 +6,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AiroportWritable implements Writable {
-    private IntWritable year , quarter , month , da
+    private IntWritable year , quarter , month , day_of_month , 
     @Override
     public void write(DataOutput out) throws IOException {
 
