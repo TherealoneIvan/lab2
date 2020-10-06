a08028314d50c34203527b12e2bfdc8cbeda5580
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 01:35:39 +0300

    Scripted auto-commit on change (2020-10-07 01:35:39) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -3,6 +3,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper extends Mapper<LongWritable, Text, TextPair , Text> {
-
+public class AirportMapper extends Mapper<LongWritable, Text, Text , Text> {
+    
 }
