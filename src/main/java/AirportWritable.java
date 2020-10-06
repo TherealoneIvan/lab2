2e6ac5f6988a01d28dccdb032d41039c175747ec
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-07 01:16:09 +0300

    Scripted auto-commit on change (2020-10-07 01:16:09) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,4 +1,17 @@
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritable implements Writable {
+    @Override
+    public void write(DataOutput out) throws IOException {
+        
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+
+    }
 }
