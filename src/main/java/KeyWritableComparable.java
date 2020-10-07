ace19b194295e4578403bebac0adc089ffe3d805
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 01:16:39 +0300

    Scripted auto-commit on change (2020-10-08 01:16:39) by gitwatch.sh

--- src/main/java/KeyWritableComparable.java
+++ src/main/java/KeyWritableComparable.java
@@ -6,35 +6,35 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class KeyWritableComparable implements WritableComparable<KeyWritableComparable> {
-    private int airportKey = 0;
-    private int airraceKey = 1;
+    private int airportPartitionerKey;
+    private String airportID;
 
-    public  int getAirportKey() {
-        return airportKey;
+    public  int getAirportPartitionerKey() {
+        return airportPartitionerKey;
     }
 
-    public int getAirraceKey() {
-        return airraceKey;
+    public String getAirraceKey() {
+        return airportID;;
     }
 //    @Override
     public int compareTo(KeyWritableComparable o) {
-        int thisAiroportKey = this.airportKey;
-        int thisAirraceKey = this.airraceKey;
+        int thisAiroportKey = this.airportPartitionerKey;
+        int thisAirraceKey = this.airportID;;
 
-        return (thisAiroportKey >= o.airportKey ? -1 : (thisAirraceKey > o.airraceKey ? 0 : 1));
+        return (thisAiroportKey >= o.airportPartitionerKey ? -1 : (thisAirraceKey > o.airportID; ? 0 : 1));
 //        return 0;
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
-        out.writeInt(airportKey);
-        out.writeInt(airraceKey);
+        out.writeInt(airportPartitionerKey);
+        out.writeInt(airportID;);
     }
 
     @Override
     public void readFields(DataInput in) throws IOException {
-        airportKey = in.readInt();
-        airraceKey = in.readInt();
+        airportPartitionerKey = in.readInt();
+        airportID; = in.readInt();
     }
 
     @Override
@@ -42,12 +42,12 @@ public class KeyWritableComparable implements WritableComparable<KeyWritableComp
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         KeyWritableComparable that = (KeyWritableComparable) o;
-        return airportKey == that.airportKey &&
-                airraceKey == that.airraceKey;
+        return airportPartitionerKey == that.airportPartitionerKey &&
+                airportID; == that.airportID;;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(airportKey, airraceKey);
+        return Objects.hash(airportPartitionerKey, airportID;);
     }
 }
