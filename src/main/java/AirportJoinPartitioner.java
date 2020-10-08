5aef650d29bf19f195be52336fcc6e56e72d9ed9
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 20:00:49 +0300

    Scripted auto-commit on change (2020-10-08 20:00:49) by gitwatch.sh

--- src/main/java/AirportJoinPartitioner.java
+++ src/main/java/AirportJoinPartitioner.java
@@ -1,2 +1,13 @@
-public class AirportJoinPartitioner extends Partitioner<Text, Text> { {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportJoinPartitioner extends Partitioner<Text, Text> {
+    public AirportJoinPartitioner() {
+        super();
+    }
+
+    @Override
+    public int getPartition(Text text, Text text2, int numPartitions) {
+        return 0;
+    }
 }
