590624e173ec28a322ed786518de3fbbf1e414aa
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 19:46:03 +0300

    Scripted auto-commit on change (2020-10-08 19:46:03) by gitwatch.sh

--- src/main/java/GroupingAirportComparatorClass.java
+++ src/main/java/GroupingAirportComparatorClass.java
@@ -1,10 +1,13 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 
 public class GroupingAirportComparatorClass {
     @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Reducer.Context context) throws
+    protected void reduce(KeyValueWritableComparable key, Iterable<Text> values, Reducer.Context context) throws
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text systemInfo = new Text(iter.next());
