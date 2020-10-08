916dbc5e13210da676c70df93daf64dc12766a25
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 13:34:19 +0300

    Scripted auto-commit on change (2020-10-08 13:34:19) by gitwatch.sh

--- src/main/java/AirraceJoinMapper.java
+++ src/main/java/AirraceJoinMapper.java
@@ -6,7 +6,7 @@ import javax.naming.Context;
 import java.io.IOException;
 
 public class AirraceJoinMapper extends Mapper<LongWritable, Text, KeyValueWritableComparable , Text> {
-
+    AirportWritable = new AirportWritable()
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
