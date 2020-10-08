fc661d88b89dfb2c4c94b97df5bb42c324159d9e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 18:16:08 +0300

    Scripted auto-commit on change (2020-10-08 18:16:08) by gitwatch.sh

--- src/main/java/AirraceJoinMapper.java
+++ src/main/java/AirraceJoinMapper.java
@@ -13,8 +13,10 @@ public class AirraceJoinMapper extends Mapper<LongWritable, Text, KeyValueWritab
         String line = value.toString();
         String[] airraces = line.split(",");
         for (int i = 0 ; i < airraces.length ; i+=2) {
-            context.write();
-             context.write(new KeyValueWritableComparable(airraces[i] , "1") , Text());
+//            context.write();
+             context.write(new KeyValueWritableComparable(airraces[i] , "1") ,
+                     new Text(airraces[i]));
+             
         }
     }
 }
