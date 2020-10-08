54a6450be64ba3dca7d592fb8a60470b6ed126b2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-09 01:30:43 +0300

    Scripted auto-commit on change (2020-10-09 01:30:43) by gitwatch.sh

--- src/main/java/AirportJoinReducer.java
+++ src/main/java/AirportJoinReducer.java
@@ -2,10 +2,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportJoinReducer extends Reducer<GroupingAirportComparatorClass , Text, Text ,Text> {
     @Override
     protected void reduce(GroupingAirportComparatorClass key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
         
     }
 
