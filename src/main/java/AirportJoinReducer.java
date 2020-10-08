cce47db625926112a992c90e9a8091dd6beb69a2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-08 23:33:05 +0300

    Scripted auto-commit on change (2020-10-08 23:33:04) by gitwatch.sh

--- src/main/java/AirportJoinReducer.java
+++ src/main/java/AirportJoinReducer.java
@@ -1,5 +1,5 @@
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportJoinReducer extends Reducer<> {
+public class AirportJoinReducer extends Reducer<GroupingAirportComparatorClass , > {
 
 }
