bcd06e142be845f8063bcef5b3f5ed09fce002bc
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 02:28:42 +0300

    Scripted auto-commit on change (2020-10-10 02:28:42) by gitwatch.sh

--- src/main/java/AirportJoinReducer.java
+++ src/main/java/AirportJoinReducer.java
@@ -4,14 +4,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class AirportJoinReducer extends Reducer<GroupingAirportComparatorClass , Text, Text ,Text> {
+public class AirportJoinReducer extends Reducer<KeyValueWritableComparable , Text, Text ,Text> {
     @Override
-    protected void reduce(GroupingAirportComparatorClass key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(KeyValueWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airraceInfo = new Text(iter.next());
         while (iter.hasNext()){
             Text airraceDelay = iter.next();
-            context.write(key. , airraceDelay);
+            context.write(new Text(String.valueOf(key.getAirportID())) , airraceDelay);
         }
     }
 
