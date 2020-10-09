10bf1597b9cef449cb2ac9ad04efc17b19fa4a10
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-09 23:42:04 +0300

    Scripted auto-commit on change (2020-10-09 23:42:03) by gitwatch.sh

--- src/main/java/AirportJoinMapper.java
+++ src/main/java/AirportJoinMapper.java
@@ -9,7 +9,7 @@ public class AirportJoinMapper extends Mapper<LongWritable, Text, KeyValueWritab
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] airraces = line.split(",");
-        for (int i = 1 ; i < airraces.length ; i+=2) {
+        for (int i = 3 ; i < airraces.length ; i+=2) {
             int airace = Integer.parseInt(airraces[i - 1]);
             context.write(new KeyValueWritableComparable(airace ,
                             1) , new Text(airraces[i]));
