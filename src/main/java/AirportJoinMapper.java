0d804645d5a7e78a1018f54b67c8496fa69ef60e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 00:54:47 +0300

    Scripted auto-commit on change (2020-10-10 00:54:46) by gitwatch.sh

--- src/main/java/AirportJoinMapper.java
+++ src/main/java/AirportJoinMapper.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 
 public class AirportJoinMapper extends Mapper<LongWritable, Text, KeyValueWritableComparable , Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        String [] airraces = line.split(",").;
+        String line = value.toString().replace("\"" , "");
+        String [] airraces = line.split(",");
         for (int i = 1 ; i < airraces.length; i++) {
-            if (airraces[0].compareTo("Code"))
-            int airaceID = Integer.parseInt(airraces[0]);
-            context.write(new KeyValueWritableComparable(airaceID ,
-                            1) , new Text(airraces[1]));
+            if (airraces[0].compareTo("Code") == 0) {
+                int airaceID = Integer.parseInt(airraces[0]);
+                context.write(new KeyValueWritableComparable(airaceID,
+                        1), new Text(airraces[1]));
+            }
         }
     }
 }
--- src/main/java/AirraceJoinMapper.java
+++ src/main/java/AirraceJoinMapper.java
@@ -16,6 +16,7 @@ public class AirraceJoinMapper extends Mapper<LongWritable, Text, KeyValueWritab
         String isDelay = oneRaceInfo[17];
         if (!isDelay.equals("0") && !isCanceld.equals("1") && !isDelay.equals("")
                 && !isCanceld.equals("")) {
+            if (oneRaceInfo[14].compareTo(""))
             int airoportID = Integer.parseInt(oneRaceInfo[14]);
             int delayTime = Integer.parseInt(isDelay);
             context.write(new KeyValueWritableComparable(airoportID, 1), new Text(isDelay));
