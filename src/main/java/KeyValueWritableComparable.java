d2155e42cd040c13a735d3f8739a07af95023653
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 02:16:39 +0300

    Scripted auto-commit on change (2020-10-10 02:16:39) by gitwatch.sh

--- src/main/java/KeyValueWritableComparable.java
+++ src/main/java/KeyValueWritableComparable.java
@@ -34,7 +34,7 @@ public class KeyValueWritableComparable implements WritableComparable<KeyValueWr
         }
         return -1;
     }
-    public 
+    public int compare()
 
     @Override
     public void write(DataOutput out) throws IOException {
