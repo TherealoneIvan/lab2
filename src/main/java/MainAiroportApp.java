75fab96c4b4bad35409c0a3ca5d25d3319dfe3eb
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-09 03:10:01 +0300

    Scripted auto-commit on change (2020-10-09 03:10:01) by gitwatch.sh

--- src/main/java/MainAiroportApp.java
+++ src/main/java/MainAiroportApp.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
 
