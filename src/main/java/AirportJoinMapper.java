b451a1634db9da752fe331e0eacb4a85dfa5f5aa
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-09 22:34:55 +0300

    Scripted auto-commit on change (2020-10-09 22:34:55) by gitwatch.sh

--- src/main/java/AirportJoinMapper.java
+++ src/main/java/AirportJoinMapper.java
@@ -11,8 +11,9 @@ public class AirportJoinMapper extends Mapper<LongWritable, Text, KeyValueWritab
 //        23
         String[] airraces = line.split(",");
         for (int i = 1 ; i < airraces.length ; i+=2) {
-            context.write(new KeyValueWritableComparable(new Text(airraces[i - 1]) ,
-                            new Text("1")) , new Text(airraces[i]));
+            int airace = Integer.parseInt(airraces[i - 1]);
+            context.write(new KeyValueWritableComparable(airace ,
+                            1) , new Text(airraces[i]));
         }
     }
 }
