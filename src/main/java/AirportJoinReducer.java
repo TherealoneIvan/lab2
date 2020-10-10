f28cc0e0323ac170a71e76f5453b64c294eabf9f
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 09:40:57 +0300

    Scripted auto-commit on change (2020-10-10 09:40:57) by gitwatch.sh

--- src/main/java/AirportJoinReducer.java
+++ src/main/java/AirportJoinReducer.java
@@ -9,23 +9,25 @@ public class AirportJoinReducer extends Reducer<KeyValueWritableComparable , Tex
     protected void reduce(KeyValueWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airport = new Text(iter.next());
-        Text airraceDelay = iter.next();
-        int cnt = 1;
-        double delay = Double.parseDouble(String.valueOf(airraceDelay));
-        double maxDelay = Double.parseDouble(String.valueOf(airraceDelay));
-        double minDelay = Double.parseDouble(String.valueOf(airraceDelay));
-        while (iter.hasNext()) {
-            airraceDelay = iter.next();
-            double tmpNewDelay = Double.parseDouble(String.valueOf(airraceDelay));
-            delay += tmpNewDelay;
-            if (maxDelay < tmpNewDelay)
-                maxDelay = tmpNewDelay;
-            if (minDelay > tmpNewDelay)
-                minDelay = tmpNewDelay;
-            cnt++;
-            context.write(airport,
-                    new Text("Min dealy : " + minDelay + " Max delay : " + maxDelay
-                            + " Delay : " + delay/cnt ) );
+        if (iter.hasNext()) {
+            Text airraceDelay = iter.next();
+            int cnt = 1;
+            double delay = Double.parseDouble(String.valueOf(airraceDelay));
+            double maxDelay = Double.parseDouble(String.valueOf(airraceDelay));
+            double minDelay = Double.parseDouble(String.valueOf(airraceDelay));
+            while (iter.hasNext()) {
+                airraceDelay = iter.next();
+                double tmpNewDelay = Double.parseDouble(String.valueOf(airraceDelay));
+                delay += tmpNewDelay;
+                if (maxDelay < tmpNewDelay)
+                    maxDelay = tmpNewDelay;
+                if (minDelay > tmpNewDelay)
+                    minDelay = tmpNewDelay;
+                cnt++;
+                context.write(airport,
+                        new Text("Min dealy : " + minDelay + " Max delay : " + maxDelay
+                                + " Delay : " + delay / cnt));
+            }
         }
     }
 }
