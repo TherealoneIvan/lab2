bafd0211a7bfe930bf57613dbcaf7ad5ad85d664
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 03:18:29 +0300

    Scripted auto-commit on change (2020-10-10 03:18:29) by gitwatch.sh

--- src/main/java/AirportJoinPartitioner.java
+++ src/main/java/AirportJoinPartitioner.java
@@ -4,6 +4,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportJoinPartitioner extends Partitioner<KeyValueWritableComparable, Text> {
     @Override
     public int getPartition(KeyValueWritableComparable keyValueWritableComparable, Text text, int numReduceTasks) {
-        return (keyValueWritableComparable.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+        return (keyValueWritableComparable.getAirportID().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
