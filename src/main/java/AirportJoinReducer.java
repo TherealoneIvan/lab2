cb87b619abe30d9b2aaa4fe828610e3ea57211a8
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-10 08:52:13 +0300

    Scripted auto-commit on change (2020-10-10 08:52:13) by gitwatch.sh

--- src/main/java/AirportJoinReducer.java
+++ src/main/java/AirportJoinReducer.java
@@ -9,11 +9,9 @@ public class AirportJoinReducer extends Reducer<KeyValueWritableComparable , Tex
     protected void reduce(KeyValueWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airraceInfo = new Text(iter.next());
-        if (iter.hasNext()) {
-            while (iter.hasNext()) {
-                Text airraceDelay = iter.next();
-                context.write(airraceInfo, airraceDelay);
-            }
+        while (iter.hasNext()) {
+            Text airraceDelay = iter.next();
+            context.write(new Text(String.valueOf(key.getAirportID())), airraceDelay);
         }
     }
 }
